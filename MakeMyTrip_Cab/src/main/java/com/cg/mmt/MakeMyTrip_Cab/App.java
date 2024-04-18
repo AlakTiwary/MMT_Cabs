a376b7bcfb0b9c4af872b945c07d0f080f9335f4
AlakTiwary <alaknanda.tiwari@capgemini.com>
2024-04-18 11:54:19 +0530

    cab

---
+++ MakeMyTrip_Cab/src/main/java/com/cg/mmt/MakeMyTrip_Cab/App.java
@@ -0,0 +1,13 @@
+package com.cg.mmt.MakeMyTrip_Cab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
